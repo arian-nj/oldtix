a497dfb0acfeee60ba05892528aa7e78d26165ea
arian <arian.aghajani01@gmail.com>
2025-02-05 10:48:26 +0330

    get rest of cards

--- game/Scenes/Game/Hokm4/States/MatchMaking.gd
+++ game/Scenes/Game/Hokm4/States/MatchMaking.gd
@@ -17,12 +17,9 @@ func _on_new_event(e:KEvent.Event)->void:
 		# print("parsed game_data in match making")
 	
 	elif e.type == KEvent.TYPE_NEW_CARD:
-		var json_data:Variant = JSON.parse_string(e.data)
-		var cards_json:Variant = json_data["cards"]
-		for card_json:Variant in cards_json:
-			card_drawer.create_card(card_json["suit"],card_json["value"])
-		card_drawer.draw_cards(card_drawer.from.global_position)
+		card_drawer.new_cards_event(e)
 		Transition.emit(self,"choose_hokm")
-	
 
+func Exit()->void:
+	ws.new_event.disconnect(_on_new_event)
 
