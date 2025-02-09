07e06f15e53f6f46a9de31008a09dddfe9f63966
arian <arian.aghajani01@gmail.com>
2025-02-09 23:02:50 +0330

    godot 4.4

--- backend/cmd/hokm2/game.go
+++ backend/cmd/hokm2/game.go
@@ -1,205 +1,43 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"math/rand"
-	"strconv"
-	"time"
-
 	cards "github.com/arian-nj/master-card/back/internal/card"
-	"github.com/arian-nj/master-card/back/internal/socket"
 )
 
-func (app *ApplicationH2) RunGame(game *GameState) error {
-	// choose hakem
-	err := app.GameInitialize(game)
-	if err != nil {
-		return err
-	}
-	for range 1 {
-		new_trick := NewTrick()
-		game.CurrentTrick = new_trick
-		game.Tricks = append(game.Tricks, game.CurrentTrick)
-
-		new_trick.HakemIndex = rand.Intn(len(game.Players))
-		for _, p := range game.Players {
-			err := app.SendGameData(game, p)
-			if err != nil {
-				return err
-			}
-		}
-
-		all_cards := cards.NewAllCards()
-		all_cards = app.sendCards(5, all_cards, game.Players)
-
-		app.WaitToChooseHokm(game)       // put hokm in game.CurrentTurn.Hokm
-		for _, p := range game.Players { // update hokm data
-			app.SendGameData(game, p)
-		}
-
-		// send rest of cards
-		all_cards = app.sendCards(4, all_cards, game.Players)
-		app.sendCards(4, all_cards, game.Players)
-
-		err = app.RunTurn(game)
-		if err != nil {
-			return err
+func isCardInCards(card *cards.Card, cards *[]cards.Card) (int, bool) {
+	for index, inHandCard := range *cards {
+		if inHandCard.Suit == card.Suit && inHandCard.Value == card.Value {
+			return index, true
 		}
 	}
-	return nil
+	return 0, false
 }
 
-func (app *ApplicationH2) GameInitialize(game *GameState) error {
-	for _, p := range game.Players {
-		app.BackgroundTask(func() error {
-			app.socketHandlers(game, p)
-			return nil
-		})
+func hasSuit(wanted_suite cards.Suite, cards *[]cards.Card) bool {
+	for _, inHandCard := range *cards {
+		if inHandCard.Suit == wanted_suite {
+			return true
+		}
 	}
-
-	return nil
+	return false
 }
 
-func (app *ApplicationH2) WaitToChooseHokm(game *GameState) {
-
-	choose_hokm_ticker := time.NewTicker(10 * time.Second)
-	defer choose_hokm_ticker.Stop()
-
-	for {
-
-		select {
-		case new_game_event := <-game.GameEventsCh:
-			if new_game_event.event.Type != socket.TypeHokmChoosed {
-				continue
-			}
-			if new_game_event.Player != game.Players[game.CurrentTrick.HakemIndex] {
-				continue
-			}
-			hokm_data := new_game_event.event.Data
-			if new_game_event.event.Data == nil {
-				app.Logger.Info("no data")
-				continue
-			}
-			app.Logger.Info("here setting")
-			hokm_int, err := strconv.Atoi(string(*hokm_data))
-			if err != nil {
-				app.Logger.Error(fmt.Sprintf("trying to set %s as hokm", string(*hokm_data)))
-				continue
-			}
-			new_hokm := cards.Suite(hokm_int)
-			game.CurrentTrick.Hokm = new_hokm
-			app.Logger.Info(fmt.Sprintf("new hokm is choosed by hakem %d ", hokm_int))
-			return
-		case <-choose_hokm_ticker.C:
-			rand_index := rand.Intn(4)
-			new_hokm := cards.AllSuits[rand_index]
-			game.CurrentTrick.Hokm = new_hokm
-			app.Logger.Info(fmt.Sprintf("new hokm is choosed by server %d ", int(new_hokm)))
-			return
-		}
+// have card
+// same suite as first card if not first move
+func (game *GameState) isMoveValid(player *Player, card *cards.Card) bool {
+	cardIndex, haveCard := isCardInCards(card, &player.Cards)
+	if !haveCard {
+		return false
 	}
-}
-
-func (app *ApplicationH2) RunTurn(game *GameState) error {
-	// new Turn
-	game.CurrentTrick.CurrentTurn = NewTurn()
-	time.Sleep(time.Second * 1)
-
-	// game starts
-	for _, p := range game.Players {
-		game_data, err := json.Marshal(game)
-		if err != nil {
-			return err
-		}
-		p.Client.Egres <- *socket.NewEvent(socket.TypeTurnStart, socket.EventMessage(game_data))
-	}
-
-	to_play_order := []*Player{}
-	starter_player_index := game.CurrentTrick.HakemIndex
-	after_ward := []*Player{}
-	for ind, p := range game.Players {
-		if ind != starter_player_index {
-			after_ward = append(after_ward, p)
-		} else {
-			to_play_order = append(to_play_order, p)
-		}
-	}
-	to_play_order = append(to_play_order, after_ward...)
-
-	for _, p := range to_play_order {
-		p.Client.Egres <- *socket.NewEvent(socket.TypeYourTurn, socket.EventMessage(""))
-		NewTicker := time.NewTicker(time.Second * 10)
-		for {
-			select {
-			case new_game_event := <-game.GameEventsCh:
-				if new_game_event.event.Type != socket.TypePlayTurn {
-					continue
-				}
-				if new_game_event.Player.UserId != p.UserId {
-					continue
-				}
-				if new_game_event.event.Data == nil {
-					continue
-				}
-
-				var card_played cards.Card
-				err := json.Unmarshal([]byte(*new_game_event.event.Data), &card_played)
-				if err != nil {
-					continue
-				}
-
-			case <-NewTicker.C:
-				NewTicker.Stop()
-
+	currentTurn := game.CurrentTrick.CurrentTurn
+	if len(currentTurn.CardsPlayed) > 0 { // if not first move
+		if hasSuit(currentTurn.CardsPlayed[0].Suit, &player.Cards) {
+			if card.Suit != currentTurn.CardsPlayed[0].Suit {
+				return false
 			}
 		}
 	}
-	return nil
-}
-
-// events come here if not used go to GameEventCh
-func (app *ApplicationH2) socketHandlers(game *GameState, p *Player) {
-	for {
-		new_event := <-p.Client.NewEvents
-		if new_event.Type == socket.TypeGetData {
-			app.SendGameData(game, p)
-		} else {
-			game.GameEventsCh <- NewGameEvent(&new_event, p)
-		}
-	}
-}
-
-func (app *ApplicationH2) SendGameData(game *GameState, p *Player) error {
-	// send game data
-	game_data, err := json.Marshal(game)
-	if err != nil {
-		return err
-	}
-	p.Client.Egres <- *socket.NewEvent(socket.TypeGameData, socket.EventMessage(game_data))
-	return nil
-}
-
-func (app *ApplicationH2) sendCards(number int, all_cards []cards.Card, players []*Player) []cards.Card {
-	for _, p := range players {
-		var randomCards []cards.Card
-		var err error
-		randomCards, all_cards, err = cards.GiveRandomCards(number, all_cards)
-		if err != nil {
-			app.Logger.Error(err.Error())
-		}
-		p.Cards = append(p.Cards, randomCards...)
-
-		var output struct {
-			NewCards []cards.Card `json:"cards"`
-		}
-		output.NewCards = randomCards
-		data_byte, err := json.Marshal(output)
-		if err != nil {
-			app.Logger.Error(err.Error())
-		}
-		p.Client.Egres <- *socket.NewEvent(socket.TypeNewCard, socket.EventMessage(data_byte))
-	}
-	return all_cards
-
+	currentTurn.CardsPlayed = append(currentTurn.CardsPlayed, player.Cards[cardIndex])
+	player.Cards = append(player.Cards[:cardIndex], player.Cards[cardIndex+1:]...)
+	return true
 }
--- game/Scenes/Game/Hokm4/table.gd
+++ game/Scenes/Game/Hokm4/table.gd
@@ -17,32 +17,3 @@ func parse_game_data(json_string:String)->void:
 	
 	print(json.data)
 	game_data = JsonClassConverter.json_to_class(GameData,json.data)
-	
-	# game_data.id = json_data["id"]
-	# # print("Hokm is ",game_data.hokm)
-	# for player:Variant in json_data["players"]:
-	# 	var p := PlayerData.new()
-	# 	p.user_id = player["user_id"]
-	# 	p.team = player["team"]
-	# 	game_data.players.append(p)
-	
-	# # turn stuff
-	# var trick_json:Variant = json_data.get("current_trick")
-	# if trick_json != null:
-	# 	game_data.current_trick.hokm = trick_json["hokm"]
-	# 	game_data.current_trick.hakem_index = trick_json["hakem_index"]
-
-	# 	var current_turn:Variant = trick_json.get("current_turn")
-	# 	if current_turn:
-	# 		var new_turn := TurnData.new()
-	# 		for card_json:Variant in current_turn["cards"]:
-	# 			var tb := CardData.new()
-	# 			tb.suite = card_json["suit"]
-	# 			tb.value = card_json["value"]
-	# 			game_data.current_trick.current_turn.cards_played.append(tb)
-	# 		game_data.current_trick.current_turn = new_turn
-	# 		print("got turn data")
-	# 	else:
-	# 		print("no turn data")
-	
-	# return true
