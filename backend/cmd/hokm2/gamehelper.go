446bebe10f7107e183035c7b6e7756f112b33c51
arian <arian.aghajani01@gmail.com>
2025-02-13 22:21:25 +0330

    can play one hand && fix not parsing

--- backend/cmd/hokm2/gamehelper.go
+++ backend/cmd/hokm2/gamehelper.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+
 	cards "github.com/arian-nj/master-card/back/internal/card"
+	"github.com/arian-nj/master-card/back/internal/socket"
 )
 
 func isCardInCards(card *cards.Card, cards *[]cards.Card) (int, bool) {
@@ -45,29 +48,74 @@ func (game *GameState) ValidateAndDoMove(player *Player, card *cards.Card) (int,
 	return cardIndex, true
 }
 
-// func (game *GameState) WhoWins() *PlayerCardPlayed {
+func (game *GameState) WhoWins() *PlayerCardPlayed {
 
-// 	HokmSuite := game.CurrentTrick.Hokm
+	HokmSuite := game.CurrentTrick.Hokm
 
-// 	Winner := game.CurrentTrick.CurrentTurn.CardsPlayed[0]
+	Winner := game.CurrentTrick.CurrentTurn.CardsPlayed[0]
+	for _, pc := range game.CurrentTrick.CurrentTurn.CardsPlayed {
+		if Winner.Card.Suit == HokmSuite {
+			if pc.Card.Suit == HokmSuite && pc.Card.Value >= Winner.Card.Value {
+				Winner = pc
+			}
+		} else {
+			if pc.Card.Suit == HokmSuite {
+				Winner = pc
+			} else if pc.Card.Suit == Winner.Card.Suit && pc.Card.Value > Winner.Card.Value {
+				Winner = pc
+			}
+		}
+	}
 
-// 	for _, pc := range game.CurrentTrick.CurrentTurn.CardsPlayed {
-// 		if Winner.Card.Suit == HokmSuite {
-// 			if pc.Card.Suit == HokmSuite && pc.Card.Value >= Winner.Card.Value {
-// 				Winner = pc
-// 			}
-// 		} else {
-// 			if pc.Card.Suit == HokmSuite {
-// 				Winner = pc
-// 			} else if pc.Card.Suit == Winner.Card.Suit && pc.Card.Value > Winner.Card.Value {
-// 				Winner = pc
-// 			}
-// 		}
-// 	}
-// 	return Winner
+	return Winner
 
-// }
+}
 
 // hokm
 // zamineh
 // number
+
+type GameStateOut struct {
+	*GameState
+	YourTeam Team `json:"your_team"`
+}
+
+func (game *GameState) SendGameData(p *Player) error {
+	gsOut := GameStateOut{
+		GameState: game,
+		YourTeam:  p.TeamId,
+	}
+	// send game data
+	game_data, err := json.Marshal(gsOut)
+	if err != nil {
+		return err
+	}
+	p.Client.Egres <- *socket.NewEvent(socket.TypeGameData, socket.EventMessage(game_data))
+	return nil
+}
+
+func (game *GameState) sendCards(number int, all_cards []cards.Card, players []*Player) []cards.Card {
+	var remaining_cards []cards.Card = all_cards
+
+	for _, p := range players {
+		var randomCards []cards.Card
+		var err error
+		randomCards, remaining_cards, err = cards.GiveRandomCards(number, remaining_cards)
+		if err != nil {
+			game.Logger.Error(err.Error())
+		}
+
+		var output struct {
+			NewCards []cards.Card `json:"cards"`
+		}
+		output.NewCards = randomCards
+		data_byte, err := json.Marshal(output)
+		if err != nil {
+			game.Logger.Error(err.Error())
+		}
+		p.Client.Egres <- *socket.NewEvent(socket.TypeNewCard, socket.EventMessage(data_byte))
+		p.Cards = append(p.Cards, randomCards...)
+	}
+	return remaining_cards
+
+}
