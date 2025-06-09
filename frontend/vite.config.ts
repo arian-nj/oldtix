93197b74980be8371867a5fbc98859f5ca00b812
arian <arian.aghajani01@gmail.com>
2025-06-09 13:22:00 +0330

    change front domain base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	base: '/telegram/.',
+	base: '/telegram/',
 	build: {
 		emptyOutDir: true,
 		outDir: '../backend/telegram/static',
